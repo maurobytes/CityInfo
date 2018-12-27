ef95194c8fd51c657f103e78cb859759770611d4
Mauricio Contreras <m_contreras@outlook.cl>
2018-12-27 17:48:27 -0300

    GetCities and GetCity

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -9,14 +9,16 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : Controller
     {
-        [HttpGet]
+        [HttpGet()]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>
-            {
-                new {id=1, Name="New York City"},
-                new {id=2, Name="Rapa Nui"}
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
         }
     }
 }

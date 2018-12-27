5870951fe2371bb620fa1ac89a6e9086c70fe70f
Mauricio Contreras <m_contreras@outlook.cl>
2018-12-27 17:20:00 -0300

    CitiesController

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new {id=1, Name="New York City"},
+                new {id=2, Name="Rapa Nui"}
+            });
+        }
+    }
+}
